0f64ba9e03f83f1f4d674e60755737b50eb33b83
DannyGrimmig <dannygrimmig17@gmail.com>
2023-05-02 23:30:33 -0400

    Landing Page: creation and structured beginning

---
+++ client/landing/langing.js
@@ -0,0 +1,28 @@
+let nav = document.getElementById("nav");
+let navCenter = document.getElementById("navCenter");
+let navright = document.getElementById("navRight");
+
+window.addEventListener('scroll', () => {
+    let y = document.body.scrollTop;
+    if(y >= 40){
+        nav.className="nav";
+        navCenter.className="chunk center";
+        navright.className="chunk right"
+        
+    }
+    else{
+        nav.className="nav transparent";
+        navCenter.className="chunk center transparent";
+        navright.className = "chunk right transparent";
+    }
+  });
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
