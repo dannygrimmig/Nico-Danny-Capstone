fd6061ebd62b577757aa58ab690c5ca1d18c3ee8
DannyGrimmig <dannygrimmig17@gmail.com>
2023-05-04 22:21:03 -0400

    landing page: hero image carosel

--- client/landing/langing.js
+++ client/landing/langing.js
@@ -26,3 +26,21 @@ window.addEventListener('scroll', () => {
         });
     });
 });
+
+setInterval(heroImage, 5000);
+let heroIndex = 0;
+let heroContainer = document.getElementById("hero");
+let heroArr = [
+    "https://allears.net/wp-content/uploads/2020/11/pooh-eating-gif.gif",
+    "https://media0.giphy.com/media/K4x1ZL36xWCf6/giphy.gif",
+    "https://i.gifer.com/origin/a5/a5569cd8a6d090e462d4a67be54302f9.gif",
+    "https://media3.giphy.com/media/iZjoLdS1nwMRq/giphy.gif",
+    "https://media0.giphy.com/media/yZaInh5c9AUZHE0ee7/giphy-downsized.gif",
+]
+function heroImage(){
+    heroIndex++;
+    if(heroIndex >= heroArr.length){
+        heroIndex = 0;
+    }
+    heroContainer.style.backgroundImage = 'url(' +heroArr[heroIndex] + ')';
+}
